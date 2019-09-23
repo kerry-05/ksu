b5e681e9dab43f595ff1c8513976d8c531086202
kerry-05 <55220520+kerry-05@users.noreply.github.com>
2019-09-23 16:24:00 +0800

    Update 默寫1.0.c

--- t/默寫1.0.c
+++ t/默寫1.0.c
@@ -1,9 +1,14 @@
 #include<stdio.h> //匯入函式庫 (io)
 #include<stdlib.h>// 匯入函式庫(lib)
 
-
+int a,b,c,sum;
 int main(void) {   //主函式(程式進入點)(entrance of the program) 
-    printf("hello,word");//列印出文字 hello word 並換行
-    system("pause");//保留畫面 
+    int a1 = 1;//使a1=1 
+    int a2 = 2;//使a2=2 
+    int a3 = 3;//使a3=3 
+    int a = a1 *a2 *a3;//使a=a1*a2*a3 
+    printf("%d*%d*%d=%d\n",a1,a2,a3,a);//格式化輸出a1*a2*a3=a 
     return 0;//回傳值	 
 } //主函式結束( end of main metnod)
+
+
