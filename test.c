9b1bd21d0b6049367df3940f5f1d150ed18d3435
kerry-05 <55220520+kerry-05@users.noreply.github.com>
2019-09-23 14:47:22 +0800

    Update test.c

--- test.c
+++ test.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>// 匯入函式庫(lib)
 
 
- int main(void) {   //主函式(程式進入點)(entrance of the program) 
-	printf("hello,word");//列印出文字 hello word 並換行
-	system("pause");//保留畫面 
-	return 0;//回傳值	 
+int main(void) {   //主函式(程式進入點)(entrance of the program) 
+    printf("hello,word");//列印出文字 hello word 並換行
+    system("pause");//保留畫面 
+    return 0;//回傳值	 
 } //主函式結束( end of main metnod)
