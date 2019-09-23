03ded3bcaaf9f2255fafb2f4e40ce626e9a0306c
kerry-05 <55220520+kerry-05@users.noreply.github.com>
2019-09-23 16:00:51 +0800

    Create 默寫.c

---
+++ t/默寫.c
@@ -0,0 +1,13 @@
+#include<stdio.h> //匯入函式庫 (io)
+#include<stdlib.h>// 匯入函式庫(lib)
+
+int a,b,c,sum;
+int main(void) {   //主函式(程式進入點)(entrance of the program) 
+    int a,b,c,sum;//列印出文字 hello word 並換行
+    a = 10;//指定值給變數a
+    b = 20;//指定值給變數b
+    c = 30;//指定值給變數c 
+    sum=a+b+c;//加總a+b+c 
+    printf("%d", sum);//輸出加總的值 
+    return 0;//回傳值	 
+} //主函式結束( end of main metnod)
